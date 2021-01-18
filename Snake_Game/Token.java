c8fd0fb1173e21feb5723a001abb88579a9cc87b
thibui0908 <thi.t.bui@sjsu.edu>
2021-01-17 22:36:33 -0800

    Uploading game files

---
+++ Snake_Game/Token.java
@@ -0,0 +1,41 @@
+package Snake;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Token {
+	private int x, y, score;
+	private Snake snake;
+	
+	public Token(Snake s) {
+		x = (int) (Math.random() * 395);
+		y = (int) (Math.random() * 395);
+		this.snake = s;
+	}
+	
+	public void changePosition() {
+		x = (int) (Math.random() * 395);
+		y = (int) (Math.random() * 395);
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void draw(Graphics g) {
+		g.setColor(Color.GREEN);
+		g.fillRect(x, y, 6, 6);	
+	}
+	
+	public boolean snakeCollision() {
+		int snakeX = snake.getX() + 2;
+		int snakeY = snake.getY() + 2;
+		if (snakeY >= y -1 && snakeY <= (y +7)) {
+			changePosition();
+			score++;
+			snake.setElongate(true);
+			return true;
+		}
+		return false;
+	}
+}
